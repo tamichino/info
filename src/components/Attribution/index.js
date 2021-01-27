ccbc02053cd6cb624bab5ec13e8eda82a6f550f0
trxgifts <latreallross@gmail.com>
2021-01-27 20:53:30 +0700

    first commit

---
+++ src/components/Attribution/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Attribution = () => (
+  <p className="attribution">
+    <a href="https://github.com/pancakeswap/pancakeswap-info" rel="noopener noreferrer" target="_blank">
+      Github
+    </a>{' '}
+    |{' '}
+    <a href="https://info.tin.finance" rel="noopener noreferrer" target="_blank">
+      Tin Finance
+    </a>{' '}
+    |{' '}
+    <a href="https://gifer.com/en/1Atv" rel="noopener noreferrer" target="_blank">
+      GIF
+    </a>
+  </p>
+)
+
+export default Attribution
